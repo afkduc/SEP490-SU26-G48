e2eac9d8f23f213b6f4332627c14c66d56f55ce9
nguyenminh732 <minhnghe187113@fpt.edu.vn>
2026-07-06 05:34:13 +0700

    be repair settlement form

--- BE/src/presentation/routes/index.js
+++ BE/src/presentation/routes/index.js
@@ -3,6 +3,7 @@ const { success } = require('../../utils/response');
 const buildUserRouter = require('./userRoutes');
 const buildAuthRouter = require('./authRoutes');
 const buildAdminRouter = require('./adminRoutes');
+const buildVehicleRouter = require('./vehicleRoutes');
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.get('/', (req, res) => {
 router.use('/auth', buildAuthRouter());
 router.use('/users', buildUserRouter());
 router.use('/admin', buildAdminRouter());
+router.use('/vehicles', buildVehicleRouter());
 
 module.exports = router;
