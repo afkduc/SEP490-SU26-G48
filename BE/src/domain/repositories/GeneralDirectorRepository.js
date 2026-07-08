6d8ae491e57d51dc3ced6b728600efd35bb17889
TuanNguyen-04 <tuannhhe181032@fpt.edu.vn>
2026-07-08 21:17:30 +0700

    Thêm full code chức năng General Director hiện tại

---
+++ BE/src/domain/repositories/GeneralDirectorRepository.js
@@ -0,0 +1,15 @@
+class GeneralDirectorRepository {
+  async listSettlementReports() {
+    throw new Error('Not implemented');
+  }
+
+  async getSettlementReportById() {
+    throw new Error('Not implemented');
+  }
+
+  async listBranches() {
+    throw new Error('Not implemented');
+  }
+}
+
+module.exports = GeneralDirectorRepository;
