41ab61b6b3fc99a7687c766c4edd925648fd4b9d
ducdeptraisieucapvutru <90845257+afkduc@users.noreply.github.com>
2026-07-08 13:34:35 +0700

    feat(inventory): add InventoryService - stock list, low-stock, adjust, summary

--- BE/src/application/services/index.js
+++ BE/src/application/services/index.js
@@ -1,7 +1,9 @@
 const UserService = require('./UserService');
 const ProductService = require('./ProductService');
+const { InventoryService } = require('./InventoryService');
 const { makeUserRepository } = require('../../infrastructure/repositories');
 const { makeProductRepository } = require('../../infrastructure/repositories');
+const { makeInventoryRepository } = require('../../infrastructure/repositories');
 
 function makeUserService() {
   return new UserService({ userRepository: makeUserRepository() });
@@ -11,9 +13,15 @@ function makeProductService() {
   return new ProductService({ productRepository: makeProductRepository() });
 }
 
+function makeInventoryService() {
+  return new InventoryService({ inventoryRepository: makeInventoryRepository() });
+}
+
 module.exports = {
   UserService,
   ProductService,
+  InventoryService,
   makeUserService,
   makeProductService,
+  makeInventoryService,
 };
