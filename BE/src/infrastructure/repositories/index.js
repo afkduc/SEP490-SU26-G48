7203d6825a7c7488b0e79b97750fd0b13a8e3ca0
ducdeptraisieucapvutru <90845257+afkduc@users.noreply.github.com>
2026-07-08 13:34:22 +0700

    feat(inventory): add InventoryRepositoryImpl with SQL Server queries

--- BE/src/infrastructure/repositories/index.js
+++ BE/src/infrastructure/repositories/index.js
@@ -1,5 +1,6 @@
 const UserRepositoryImpl = require('./UserRepositoryImpl');
 const ProductRepositoryImpl = require('./ProductRepositoryImpl');
+const InventoryRepositoryImpl = require('./InventoryRepositoryImpl');
 
 function makeUserRepository() {
   return new UserRepositoryImpl();
@@ -9,7 +10,12 @@ function makeProductRepository() {
   return new ProductRepositoryImpl();
 }
 
+function makeInventoryRepository() {
+  return new InventoryRepositoryImpl();
+}
+
 module.exports = {
   makeUserRepository,
   makeProductRepository,
+  makeInventoryRepository,
 };
