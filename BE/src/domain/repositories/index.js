b92e244ffd4f8a9de121c7a3136bbef076567c2d
ducdeptraisieucapvutru <90845257+afkduc@users.noreply.github.com>
2026-07-08 13:34:09 +0700

    feat(inventory): add InventoryRepository interface

--- BE/src/domain/repositories/index.js
+++ BE/src/domain/repositories/index.js
@@ -1,7 +1,9 @@
 const UserRepository = require('./UserRepository');
 const ProductRepository = require('./ProductRepository');
+const InventoryRepository = require('./InventoryRepository');
 
 module.exports = {
   UserRepository,
   ProductRepository,
+  InventoryRepository,
 };
