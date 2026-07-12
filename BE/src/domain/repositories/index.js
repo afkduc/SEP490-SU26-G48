eb68fead5a9cdca2d27fd36f9cb0fac39210abd9
afkduc <afkduc.id@gmail.com>
2026-07-13 03:30:23 +0700

    feat(be): them Supplier module (read-only)

--- BE/src/domain/repositories/index.js
+++ BE/src/domain/repositories/index.js
@@ -1,9 +1,11 @@
 const UserRepository = require('./UserRepository');
 const ProductRepository = require('./ProductRepository');
 const InventoryRepository = require('./InventoryRepository');
+const SupplierRepository = require('./SupplierRepository');
 
 module.exports = {
   UserRepository,
   ProductRepository,
   InventoryRepository,
+  SupplierRepository,
 };
