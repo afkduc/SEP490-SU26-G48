9278d41a78c61c83d03b530a895d0259bbf28b25
afkduc <afkduc.id@gmail.com>
2026-07-16 04:06:27 +0700

    feat(export-request): sql impl + wire into repository registry

--- BE/src/infrastructure/repositories/index.js
+++ BE/src/infrastructure/repositories/index.js
@@ -6,6 +6,7 @@ const InventoryRepositoryImpl = require('./InventoryRepositoryImpl');
 const CustomerRepositoryImpl = require('./CustomerRepositoryImpl');
 const SupplierRepositoryImpl = require('./SupplierRepositoryImpl');
 const ImportRequestRepositoryImpl = require('./ImportRequestRepositoryImpl');
+const ExportRequestRepositoryImpl = require('./ExportRequestRepositoryImpl');
 
 function makeUserRepository() {
   return new UserRepositoryImpl();
@@ -39,6 +40,10 @@ function makeImportRequestRepository() {
   return new ImportRequestRepositoryImpl();
 }
 
+function makeExportRequestRepository() {
+  return new ExportRequestRepositoryImpl();
+}
+
 module.exports = {
   makeUserRepository,
   makeProductRepository,
@@ -48,4 +53,5 @@ module.exports = {
   makeCustomerRepository,
   makeSupplierRepository,
   makeImportRequestRepository,
+  makeExportRequestRepository,
 };
