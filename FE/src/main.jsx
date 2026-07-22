423ec282fe53ea655f19cadebe827440de6b9b9c
sonnnhe171975-ui <sonnnhe171975@fpt.edu.vn>
2026-07-23 04:27:00 +0700

    feat(permission-events): SSE realtime + matrix save flow
    - BE: PermissionEvents event bus (in-memory pub/sub)
    - BE: RoleService.setRolePermissionsMatrix emit invalidate event
      cho moi user bi anh huong (cache busting)
    - BE: RoleRepositoryImpl.toggleStatus/setRolePermissions emit user-updated
    - BE: /api/auth/refresh-permissions route - FE goi sau khi nhan SSE
    - BE: /api/events/permission-updates SSE endpoint (EventSource stream)
    - BE: sqlServer transaction helpers cho matrix save (atomic)
    - FE: usePermissionEventsSSE hook (auto reconnect, exponential backoff)
    - FE: AppContext subscribe permission-updates -> refresh permissions
    - FE: Toast thong bao "Quyen cua ban vua duoc cap nhat"
    - FE: AppRoutes mount SSE hook o root (toan app)

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider, PermissionProvider } from './contexts';
+import { ToastProvider } from './components/common/ToastContext';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProvider>
-        <PermissionProvider>
-          <App />
-        </PermissionProvider>
-      </AppProvider>
+      {/* ToastProvider phai O NGOAI AppProvider vi PermissionEventsRunner
+          (render boi AppProvider) se goi useToast() de hien toast refresh. */}
+      <ToastProvider>
+        <AppProvider>
+          <PermissionProvider>
+            <App />
+          </PermissionProvider>
+        </AppProvider>
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
